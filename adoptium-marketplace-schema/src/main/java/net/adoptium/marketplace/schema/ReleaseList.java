51aaeded8539236df1d77bc07943986ba1faeae3
John Oliver <1615532+johnoliver@users.noreply.github.com>
2022-01-20 01:37:13 +0000

    Ensure ReleaseList is added to api

--- adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/ReleaseList.java
+++ adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/ReleaseList.java
@@ -2,9 +2,11 @@ package net.adoptium.marketplace.schema;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import java.util.List;
 
+@Schema
 public class ReleaseList {
 
     private final List<Release> releases;
